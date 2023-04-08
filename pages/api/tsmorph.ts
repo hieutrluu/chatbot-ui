04507244fcb969c6b44b54c57b0a2b929f3544d9
hieutrluu <thl1g15@outlook.com>
2023-04-08 15:30:10 +0700

    TBC

--- pages/api/tsmorph.ts
+++ pages/api/tsmorph.ts
@@ -1,4 +1,4 @@
-import { Project, PropertySignature } from 'ts-morph';
+import { Project, PropertySignature, InterfaceDeclaration } from 'ts-morph';
 
 // function isRequiredProperty(propertySignature: PropertySignature): boolean {
 //   const isOptional = (propertySignature.getQuestionTokenNode() !== undefined);
@@ -10,7 +10,24 @@ const FRAPPE_BASE_URL = 'http://vector.localhost:8000';
 const API_KEY = '9b60b6f9d13b863';
 const API_SECRET = 'd6a6a5999c7a794';
 
+interface DocField {
+  fieldname: string,
+  fieldtype: string,
+  label: string,
+  reqd: boolean,
+  options?: string,
+  property?: PropertySignature,
+}
+
 async function createDocType(doctypeName: any, moduleName: any, fields:any) {
+  const docFields: DocField[] = fields.map((field: any) => ({
+    fieldname: field.fieldname,
+    fieldtype: field.fieldtype,
+    label: field.label,
+    reqd: field.reqd || false,
+    options: field.options || "",
+  }));
+
   const response = await fetch(`${FRAPPE_BASE_URL}/api/resource/DocType`, {
     method: "POST",
     headers: {
@@ -23,13 +40,7 @@ async function createDocType(doctypeName: any, moduleName: any, fields:any) {
         doctype: "DocType",
         // name: doctypeName,
         module: moduleName,
-        fields: fields.map((field: any) => ({
-          fieldname: field.fieldname,
-          fieldtype: field.fieldtype,
-          label: field.label,
-          reqd: field.reqd || false,
-          options: field.options || "",
-        })),
+        fields: docFields,
       }),
     }),
   });
@@ -50,43 +61,40 @@ const frappe = new FrappeApp(FRAPPE_BASE_URL, {
 });
 const db = frappe.db();
 
-function isDoctypeExist(prop: PropertySignature): boolean {
-  const type = prop.getType().getText();
-  console.log('type', type);
-  // db.getDoc('Docype', type)
-  //   .then((res) => {
-  //     console.log(res);
-  //     // return false;
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //     // return false;
-  //   })
-  //   .finally(() => {
-  //     // return false;
-  //   });
-  return false;
-  // const isDoctype = type.includes("Doctype");
-  // return isDoctype;
-}
+// function isDoctypeExist(prop: PropertySignature): boolean {
+//   const type = prop.getType().getText();
+//   console.log('type', type);
+//   // try{
+//   //   // db.get
+//   // }
+//   // const isDoctype = type.includes("Doctype");
+//   // return isDoctype;
+// }
 
 function createDocFieldFromProperty(prop: PropertySignature): any {
   const type = prop.getType().getText();
   let docfield_type = getTypeScriptTypeToFieldtype(type);
-  let docfield = {
+  let docfield: DocField = {
     fieldname: prop.getName(),
     label: prop.getName().toLowerCase(),
     fieldtype: docfield_type,
-    reqd: prop.hasQuestionToken() ? 0 : 1,
+    reqd: prop.hasQuestionToken() ? false : true,
     options: "",
   }
   if (docfield_type === 'Link'){
     docfield['options'] = type.split('.')[1];
+    docfield['property'] = prop;
+
+    // update doctype
   } else if (docfield_type === 'Table') {
     let childtable_doctype = isListProp(prop);
     if (childtable_doctype){
       docfield['options'] = childtable_doctype.split('.')[1].split('[]')[0];
+      docfield['property'] = prop;
     }
+
+    // update doctype
+
   }
   return docfield;
 }
@@ -139,6 +147,19 @@ function isPrimitive(prop: PropertySignature): boolean {
   return isPrimitive;
 }
 
+async function creatDocTypeFromInterface(iface: InterfaceDeclaration, is_childtable: boolean): Promise<JSON>{
+  let properties = iface.getProperties()
+  let docfields = properties.map(prop => createDocFieldFromProperty(prop));
+  let link_and_table_dt = docfields.filter(field => field.fieldtype === 'Link' || field.fieldtype === 'Table');
+  return frappe.call().post('frappe_prompt.api.utils.create_doctype', 
+    {"data": {
+      "doctype_name": iface.getName(), "module": "Frappe Prompt", "fields": docfields,
+      "is_childtable": is_childtable
+    }}
+  )
+  // .then((result) => console.log(result)).catch((error) => console.error(error));
+}
+
 const project = new Project({
   tsConfigFilePath: '/Users/luuhieu/htl-code/chatbot-ui/tsconfig.json',
 });
@@ -147,24 +168,15 @@ let srcFiles = project.addSourceFilesAtPaths(
   '/Users/luuhieu/htl-code/chatbot-ui/types/*{.d.ts,.ts}',
 );
 let interfaces = srcFiles[0].getInterfaces();
+
 interfaces.forEach((iface) => {
-  if (iface.getName() === 'Conversation'){
-    console.log(iface.getName());
-    let properties = iface.getProperties();
-    let docfields = properties.map(prop => createDocFieldFromProperty(prop));
-    console.log(docfields)
-    // frappe.call().post('frappe_prompt.frappe_prompt.doctype.module.utils.my_function', {"data": {"test": "test"}}
-    frappe.call().post('frappe_prompt.api.utils.create_doctype', {"data": {"doctype_name": iface.getName(), "module": "Frappe Prompt", "fields": docfields}}
-    ).then((result) => console.log(result)).catch((error) => console.error(error));
-    // createDocType('Conversation', 'Frappe Prompt', properties.map(prop => createDocFieldFromProperty(prop)));
-    // db.createDoc('DocType',{
-    //     __newname: 'Conversation',
-    //     module: 'Frappe Prompt',
-    //     fields: properties.map(prop => createDocFieldFromProperty(prop))
-    // }).then((res) => {
-    //   console.log(res);
-    // }).catch(err => console.error(err)).finally(() => console.log('done'));
-  }
+  creatDocTypeFromInterface(iface);
+  // if (iface.getName() === 'Conversation'){
+  //   console.log(iface.getName());
+  //   let properties = iface.getProperties();
+    
+    
+  // }
   // properties.forEach((property) => {
   //   console.log(property.getName());
   //   console.log(property.getType().getText());
